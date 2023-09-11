1ced948321e26947d03bf1488b7e138460b5923c
briaar <briarusss@gmail.com>
2023-09-11 16:55:57 -0400

    trying to correct any errors, added some for select featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,43 @@ import viteLogo from '/vite.svg'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  // null so you can conditionally render an element in your JSX code
+
+  console.log("puppyList: ", puppyList);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-     
+      <div className="App">
+        <h1> Puppy List </h1>
+      
+      {
+        puppies.map((puppy) => {
+          return <p> {puppy.name} </p>
+          return <p onClick={handleClick} key ={puppy.id}> {puppy.name} </p>
+        })
+      };
+      { featPupId && (
+        <div className="featPup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      
+      </div>
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
